921acb02caa699641e89c0328e64ee0bf780ff47
Noam Avitan <noamavitan159753@gmail.com>
2024-04-11 22:42:13 +0300

    added

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -1,31 +1,30 @@
-# orders/models.py
 from django.db import models
+from typing import List
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
+    name: str = models.CharField(max_length=100)
+    email: str = models.EmailField()
+    phone: str = models.CharField(max_length=20)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    type = models.CharField(max_length=20)
+    name: str = models.CharField(max_length=100)
+    price: float = models.DecimalField(max_digits=10, decimal_places=2)
+    type: str = models.CharField(max_length=20)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 class Order(models.Model):
-    order_number = models.CharField(max_length=20)
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
-    comments = models.TextField(blank=True)
-    last_update_date = models.DateField(auto_now=True)
-    creation_date = models.DateField(auto_now_add=True)
-    products = models.ManyToManyField(Product)
+    order_number: str = models.CharField(max_length=20)
+    customer: Customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date: str = models.DateField(auto_now_add=True)
+    comments: str = models.TextField(blank=True)
+    last_update_date: str = models.DateField(auto_now=True)
+    creation_date: str = models.DateField(auto_now_add=True)
+    products: List[Product] = models.ManyToManyField(Product)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.order_number
-
